16f8a88a3e4370c3f93e5f11ee7a3a8ab89f7401
Zenna Tavares <zennatavares@gmail.com>
2019-10-04 11:59:43 -0400

    update runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,3 @@
 using MiniOmega, Statistics
 
-"Bernoulli Distribution"
-bernoulli(ω) = ω[1] > 0.5
-
-function f(ω)
-  x = bernoulli(ω)
-  y = bernoulli(ω)
-end
-
-function procedure()
-  x = rand()
-  y = rand()
-  (x, y)
-end
+include("rifleman.jl")
