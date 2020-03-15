0f560bffdd5a9ddf4fdc616cacfa8716b420d70d
Zenna Tavares <Zennatavares@gmail.com>
2020-03-15 14:25:32 -0400

    fix pointwise

--- src/pointwise.jl
+++ src/pointwise.jl
@@ -1,39 +1,4 @@
-module Pointwise
-
-# This is a bad idea..
-# to haev pointwise depend on global state
-
-
-export pw, ==ₚ
-# using ..OmegaCore: defΩ
-
-# methodexists(f::Type{F}, x::Type{X}, y::Type{Y}) where {F <: Function, X, Y} =
-#   length(methods(F.instance, (X, Y))) > 0
-
-# methodexists(f::Type{F}, x::Type{X}) where {F <: Function, X} =
-#   length(methods(F.instance, (X,))) > 0
-
-# methodexists(f::Type{F}, x::Type{X}) where {F, X} =
-#   length(methods(F, (X,))) > 0
-
-# methodexists(f::Type{D}, x::Type{X}, y::Type{Y}) where {D, X, Y} =
-#     length(methods(D, (X, Y))) > 0
-
-# String(x, y)
-# "adada"(x, y)
-
-# @inline @generated function apply(f, ω)
-#   if methodexists(f, ω)
-#     :(f(ω))
-#   else
-#     :(f)
-#   end
-# end
-
-# pw(f, x, y) = f(apply(x, ω), apply(y, ω))
-
-
-# (::PointWise)(ω) = 
+export pw, ==ₚ, l, dl
 
 """
 Pointwise application.
@@ -50,53 +15,62 @@ do pointwise application.
 Is it safe to check whether a method exists?
 
 ```
-x1 = 3 + 2
-x2 = pw(+, 3, 2)
-@assert x1 == x2
-
-x(ω) = unif(ω)
+using Distributions
+x(ω) = Uniform(ω, 0, 1)
 y = pw(+, x, 4)
+
+f(ω) = Bool(Bernoulli(ω, 0.5)) ? sqrt : sin
+sample(pw(map, f, [0, 1, 2]))
+sample(pw(map, sqrt, [0, 1, 2]))
+sample(pw(map, dl(sqrt), [0, 1, 2]))
+sample(pw(l(f), 3))
 ```
 """
 function pw end
 
-# @generated function pw(f, args...)
-#   exists = [methodexists(x, defΩ()) for x in args]
-#   if any(exists)
-#     mod_args = [exist ? :(args[$i]) : :(args[$i](ω)) for (i, exist) in enumerate(exists)]
-#     o = :(ω -> f($(mod_args...)))
-#     Core.println(o)
-#     o
-#   else
-#     :(f(args...))
-#   end
-# end
+abstract type ABox end
 
-@inline constapply(f, ω) = f
-@inline constapply(f::DataType, ω) = f(ω)
-@inline constapply(f::Function, ω) = f(ω)
+struct LiftBox{T} <: ABox
+  val::T
+end
+"`l(x)` constructs object that indicates that `x` should be applied pointwise.  See `pw`"
+l(x) = LiftBox(x)
+
+struct DontLiftBox{T} <: ABox
+  val::T
+end
+"`dl(x)` constructs object that indicates that `x` should be not applied pointwise.  See `pw`"
+dl(x) = DontLiftBox(x)
+
+# Trait functions
+struct Lift end
+struct DontLift end
+
+traitlift(::Type{T}) where T  = DontLift()
+traitlift(::Type{<:Function}) = Lift()
+traitlift(::Type{<:DataType}) = DontLift()
+traitlift(::Type{<:LiftBox}) = Lift()
+traitlift(::Type{<:DontLiftBox}) = DontLift()
+
+@inline constapply(f::T, ω) where T = constapply(traitlift(T), f, ω)
+@inline constapply(::DontLift, f, ω) = f
+@inline constapply(::DontLift, f::ABox, ω) = f.val
+@inline constapply(::Lift, f, ω) = f(ω)
+@inline constapply(::Lift, f::ABox, ω) = (f.val)(ω)
+@inline constapply(::Lift, f::ABox, ω::ABox) = (f.val)(ω.val)
+@inline constapply(::Lift, f, ω::ABox) = f(ω.val)
 @inline pw(f, x) = ω -> f(constapply(x, ω))
+
 @inline pw(f, x1, x2) = ω -> f(constapply(x1, ω), constapply(x2, ω))
 @inline pw(f, x1, x2, x3) = ω -> f(constapply(x1, ω), constapply(x2, ω), constapply(x1, ω), constapply(x3, ω))
 @inline pw(f, x1, args...) = ω -> f(constapply(x1, ω), (constapply(x, ω) for x in args)...)
 
-# using Base.Iterators: product, filter
+@inline pw(f::LiftBox, x1, x2) = ω -> (constapply(f, ω))(constapply(x1, ω), constapply(x2, ω))
+@inline pw(f::LiftBox, x1, x2, x3) = ω -> (constapply(f, ω))(constapply(x1, ω), constapply(x2, ω), constapply(x1, ω), constapply(x3, ω))
+@inline pw(f::LiftBox, x1, args...) = ω -> (constapply(f, ω))(constapply(x1, ω), (constapply(x, ω) for x in args)...)
 
-# const MAXPOINTWISEARITY = 4
-# const Bools = (true, false)
-# for i = 1:MAXPOINTWISEARITY
-#   for comb in filter(any, product((Bools for j = 1:i)...))
-#     display(comb)
-#   end
-#   println()
-# end
-
-# # Primitives
 
 export ==ₚ, >=ₚ, <=ₚ
 @inline x ==ₚ y = pw(==, x, y)
 @inline x >=ₚ y = pw(>>, x, y)
 @inline x <=ₚ y = pw(<=, x, y)
-
-
-end
