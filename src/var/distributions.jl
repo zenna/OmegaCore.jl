41a290b7fadf5768297d5f2f40a0896103291694
Zenna Tavares <zennatavares@gmail.com>
2020-06-19 23:49:28 -0400

    invert

--- src/var/distributions.jl
+++ src/var/distributions.jl
@@ -8,3 +8,19 @@ end
 # Todo, specialise this to 1,2,3,4,5 arguments
 (m::Model)(ω) = m.x((liftapply(p, ω) for p in m.params)...)(ω)  
 (x::Type{<:Distribution})(params...) = Model(x, params)
+
+const StdNormal = Normal(0, 1)
+
+"Like a Normal Distribution but more normal"
+struct Ordinary{MU, SIG}
+  μ::MU
+  σ::SIG
+end
+
+(o::Ordinary)(ω) = (StdNormal(ω) + o.μ) * o.σ
+
+"If output of `o` is `val` what must its noise parameter must have been?`"
+invert(::Ordinary, val) = (val / o.σ) - o.μ
+
+## When we're executing o(ω), if the variable is conditioned then we can
+## update ω with the appropriate value for the standard normal
