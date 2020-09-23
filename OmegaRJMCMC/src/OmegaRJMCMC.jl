8ef68c6fbe42c6c8c91a896d7ee40cfd90913102
Zenna Tavares <zennatavares@gmail.com>
2020-09-23 15:59:45 -0400

    rjmcmc

--- OmegaRJMCMC/src/OmegaRJMCMC.jl
+++ OmegaRJMCMC/src/OmegaRJMCMC.jl
@@ -1,39 +1,43 @@
 "Reversible Jump MCMC"
 module OmegaRJMCMC
 
+using OmegaCore
+using LinearAlgebra: det
+using ForwardDiff: jacobian
+
 export rjmcmc, RJMCMC
 
 struct RJMCMCAlg end
 const RJMCMC = RJMCMCAlg()
 
-# Reversible Jump MCMC is an MCMC method designed for methods with a variable number of parameters.
-# This could be when we have a space of different models and a discrete random variable that indiciate into it.
-# Or it could be when we have choices in our model which lead to more or fewer other random variables being instantiated.
 
 """
+Reversible Jump MCMC is an MCMC method designed for models where the number
+of parameters is unknown.  These are sometimes called open-world or trans-
+dimensional models.
+
 # Arguments
-- `rng`: AbstractRng used to sample proposals in MH loop
-- `moves`: set of moves
-- `logdensity`: Density to sample from
-- `n`: number of samples  
+- `rng::AbstractRNG`:  used to sample proposals in MH loop
+- `x` : Initial state
+- `moves`: Conditional random variable over moves
+- `ℓ`: Function `ℓ(x::X)::Real` Density to sample from
+- `n`: number of samples
 
 # Returns
-- 'xs': set of samples
-
-```
-
-```
+- 'xs': set of samples distributed according to `ℓ`
 """
-function rjmcmc(rng, x, moves, logdensity, n)
-  # TODO accept jacobian
+function rjmcmc(rng, x, moves, ℓ, n)
   xs = [x]
 
   # Sample a moveset
-  m = move(rng, x) # TODO: THis is invalid
+  m = moves(rng, x) # TODO: THis is invalid
   for i = 1:n
-    (x_, u_) = m(x)    
-    ℓratio = ℓ(x') / ℓ(x)
+    u = g(ω, m)
+    (x_, u_) = m(x, u)
+    ℓratio = ℓ(x_) / ℓ(x)
     jratio = j(m, x)
+    jac = abs(det(jacobian(m)))
+    accept_ratio - ℓratio * jratio * jac
     if rand(rng, Bernoulli(accept_ratio))
       x = x'
     end
